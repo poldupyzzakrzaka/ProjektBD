6928566a0ea2243414e27a75d336b626a158aac6
Ingu <ingukk@gmail.com>
2012-05-07 01:04:00 +0200

    +wstepne logowanie

--- ProjektBD/LoginControl.xaml.cs
+++ ProjektBD/LoginControl.xaml.cs
@@ -11,17 +11,81 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace ProjektBD
 {
     /// <summary>
     /// Interaction logic for LoginControl.xaml
     /// </summary>
+
     public partial class LoginControl : UserControl
     {
+        int userId;
+        int userLevel;
+        String userName;
+        String userSurname;
+
+        public delegate void LoggedInEventHandler(object sender, LoggedInEventArgs fe);
+        public event LoggedInEventHandler LoggedInEvent;
+
         public LoginControl()
         {
             InitializeComponent();
         }
+
+        public void Zaloguj(String name, String password)
+        {
+            if (name.Length > 0 && password.Length > 0)
+            {
+                string MyConString = "SERVER=localhost;" +
+                "DATABASE=projektBD;" +
+                "UID=root;" +
+                "PASSWORD=;";
+                MySqlConnection connection = new MySqlConnection(MyConString);
+                MySqlCommand command = connection.CreateCommand();
+                MySqlDataReader Reader;
+                command.CommandText = "select p.level, u.uid, u.name, u.surname from users u, privilages p where p.uid = u.uid and u.login = \"" + textBoxLogin.Text + "\" and u.password = \"" + textBoxPassword.Password + "\"";
+                connection.Open();
+                Reader = command.ExecuteReader();
+                if(Reader.Read())
+                {
+                    userId = Reader.GetInt32(0);
+                    userLevel = Reader.GetInt32(1);
+                    userName = Reader.GetString(2);
+                    userSurname = Reader.GetString(3);
+                }
+                connection.Close();
+
+                Visibility = Visibility.Collapsed;
+
+                LoggedInEventArgs LoggedInArgs = new LoggedInEventArgs(userId, userLevel, userName, userSurname);
+                LoggedInEvent(this, LoggedInArgs);
+            }
+            else
+            {
+                //nie podane haslo/nick
+            }
+        }
+
+        private void buttonLoginConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            Zaloguj(textBoxLogin.Text, textBoxPassword.Password);
+        }
+    }
+
+    public class LoggedInEventArgs : EventArgs
+    {
+        public LoggedInEventArgs(int Id, int Level, String Name, String Surname)
+        {
+            userId = Id;
+            userLevel = Level;
+            userName = Name;
+            userSurname = Surname;
+        }
+        public int userId;
+        public int userLevel;
+        public String userName;
+        public String userSurname;
     }
 }
--- ProjektBD/MainWindow.xaml.cs
+++ ProjektBD/MainWindow.xaml.cs
@@ -22,27 +22,55 @@ namespace ProjektBD
     /// </summary>
     public partial class MainWindow : Window
     {
+        int userLevel = -1; // -1 - nie zalogowany
+
         public MainWindow()
         {
             InitializeComponent();
-            string MyConString = "SERVER=localhost;" +
-                "DATABASE=projektBD;" +
-                "UID=root;" +
-                "PASSWORD=;";
-            MySqlConnection connection = new MySqlConnection(MyConString);
-            MySqlCommand command = connection.CreateCommand();
-            MySqlDataReader Reader;
-            command.CommandText = "select * from users";
-            connection.Open();
-            Reader = command.ExecuteReader();
-            while (Reader.Read())
+            RefreshLeftButtonMenu();
+        }
+
+        public void RefreshLeftButtonMenu()
+        {
+            //tu ustawiamy wszystkie przyciski dla konkretnego zalogowanego typa
+            switch (userLevel)
             {
-                string thisrow = "";
-                for (int i = 0; i < Reader.FieldCount; i++)
-                    thisrow += Reader.GetValue(i).ToString() + ",";
-                listBox1.Items.Add(thisrow);
+                //nie zalogowany
+                case -1: break;
+                //gosc - tak sobie, raczej nie uzywany
+                case 0: break;
+                //asystentka
+                case 1: break;
+                //kierownik
+                case 2: break;
+                //specjalista
+                case 3: break;
+                //administrator
+                case 4: break;
+                //deweloper - posiada dostep do wszystkiego
+                case 5: break;
             }
-            connection.Close();
+        }
+
+        private void LoginControl1_LoggedInEvent(object sender, LoggedInEventArgs fe)
+        {
+            labelNotLoggedIn.Visibility = Visibility.Collapsed;
+            labelLoggedAs.Visibility = Visibility.Visible;
+            labelUserNameSurname.Visibility = Visibility.Visible;
+            labelLogOut.Visibility = Visibility.Visible;
+            labelUserLevel.Visibility = Visibility.Visible;
+
+            labelUserNameSurname.Content = fe.userName + " " + fe.userSurname;
+
+            switch (fe.userLevel)
+            {
+                case 0: labelUserLevel.Content = "Gość"; break;
+                case 1: labelUserLevel.Content = "Asysten(ka)"; break;
+                case 2: labelUserLevel.Content = "Kierownik"; break;
+                case 3: labelUserLevel.Content = "Specjalista"; break;
+                case 4: labelUserLevel.Content = "Administrator"; break;
+                case 5: labelUserLevel.Content = "Deweloper"; break; 
+            }
         }
     }
 }
