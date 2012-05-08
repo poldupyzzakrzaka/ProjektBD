777abffcdf3485260f397d77226f03691ece72b8
Ingu <ingukk@gmail.com>
2012-05-08 17:40:56 +0200

    -usunieta baza danych z repo
    +prawie skonczone dodawanie rekrutacji

--- ProjektBD/ExecutiveAddNewRecruitment.xaml.cs
+++ ProjektBD/ExecutiveAddNewRecruitment.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace ProjektBD
 {
@@ -22,6 +23,37 @@ namespace ProjektBD
         public ExecutiveAddNewRecruitment()
         {
             InitializeComponent();
+
+            string MyConString = "SERVER=localhost;" +
+                "DATABASE=projektBD;" +
+                "UID=root;" +
+                "PASSWORD=;";
+            MySqlConnection connection = new MySqlConnection(MyConString);
+            MySqlCommand command = connection.CreateCommand();
+            MySqlDataReader Reader;
+            command.CommandText = "select * from specialization_type";
+            connection.Open();
+            Reader = command.ExecuteReader();
+            int licznik = 0;
+            while (Reader.Read())
+            {
+                if (licznik % 3 == 0)
+                {
+                    GridLengthConverter myGridLengthConverter = new GridLengthConverter();
+                    GridLength gl1 = (GridLength)myGridLengthConverter.ConvertFromString("30*");
+                    Grid1.RowDefinitions.Add(new RowDefinition() { Height = gl1 });
+                }
+
+                int id = Reader.GetInt32(0);
+                string name = Reader.GetString(1);
+                CheckBox nowy = new CheckBox() { Content = name, Name = name + id.ToString() };
+                Grid.SetColumn(nowy, licznik%3);
+                Grid.SetRow(nowy, licznik/3);
+                licznik++;
+                Grid1.Children.Add(nowy);
+            }
+            connection.Close();
+            
         }
     }
 }
